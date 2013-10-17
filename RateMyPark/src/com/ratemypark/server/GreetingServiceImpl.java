49f6a966afa534dfadaf36d7db60253a7215751f
Jeremy Ko <jeremyko193@gmail.com>
2013-10-17 14:52:33 -0700

    Test commit

--- RateMyPark/src/com/ratemypark/server/GreetingServiceImpl.java
+++ RateMyPark/src/com/ratemypark/server/GreetingServiceImpl.java
@@ -16,6 +16,7 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		if (!FieldVerifier.isValidName(input)) {
 			// If the input is not valid, throw an IllegalArgumentException back to
 			// the client.
+			// Test
 			throw new IllegalArgumentException(
 					"Name must be at least 4 characters long");
 		}
