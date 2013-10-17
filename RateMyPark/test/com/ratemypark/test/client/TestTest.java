898e7db85882724ede18f24f501242e348fac530
iamwillz <william.zhang.285@gmail.com>
2013-10-17 14:30:43 -0700

    Adding a test test class, so that the test directory isn't empty

---
+++ RateMyPark/test/com/ratemypark/test/client/TestTest.java
@@ -0,0 +1,14 @@
+package com.ratemypark.test.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
