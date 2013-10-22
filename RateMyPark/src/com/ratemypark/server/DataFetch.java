bececabb7e2a081f8d72ac1173ce21f416227c84
jwongc <jeffreywongc@yahoo.com>
2013-10-22 15:20:17 -0700

    Redone Data Fetch to use HTTP as GWT does not support FTP retrieval

--- RateMyPark/src/com/ratemypark/server/DataFetch.java
+++ RateMyPark/src/com/ratemypark/server/DataFetch.java
@@ -1,9 +1,38 @@
 package com.ratemypark.server;
 
-import org.apache.commons.net.ftp.FTPClient;
-import java.io.IOException;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.net.*;
 
 public class DataFetch {
-
+	
+	public DataFetch() {
+		
+	}
+	
+	public String getHTML() {
+		URL url;
+		HttpURLConnection connection;
+		BufferedReader rd;
+		String line;
+		String result = "";
+		try {
+			url = new URL("http://www.vcheng.org/parks_facilities.xml");
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			rd = new BufferedReader(
+					new InputStreamReader(connection.getInputStream()));
+			while ((line = rd.readLine()) != null) {
+				result += line;
+			}
+			rd.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public static void main() {
+		DataFetch df = new DataFetch();
+		System.out.println(df.getHTML());
+	}
 }
