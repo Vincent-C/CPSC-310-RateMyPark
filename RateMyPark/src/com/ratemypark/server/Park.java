375f0dd1dc6b65fcd35af32ab011bc8beb138552
Vincent Cheng <Vincentc1208@gmail.com>
2013-10-24 15:09:54 -0700

    Use CPSC210 Coordinate class instead of the version from google maps' API

--- RateMyPark/src/com/ratemypark/server/Park.java
+++ RateMyPark/src/com/ratemypark/server/Park.java
@@ -6,7 +6,7 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
-import com.google.api.services.coordinate.Coordinate;
+//import com.google.api.services.coordinate.Coordinate;
 
 
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
