95294936604559c91811a0c62681914f2d62d9d5
Jeremy Ko <jeremyko193@gmail.com>
2013-11-01 14:00:51 -0700

    Checking in missing file from last checkin

---
+++ RateMyPark/src/com/ratemypark/client/LoginInfo.java
@@ -0,0 +1,48 @@
+package com.ratemypark.client;
+
+import java.io.Serializable;
+
+public class LoginInfo implements Serializable {
+	private String username;
+	private String sessionID;
+	private String firstName;
+	private String lastName;
+	
+	public LoginInfo() {
+		
+	}
+	
+	public LoginInfo(String username, String session) {
+		this.username = username;
+		this.sessionID = session;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getSessionId() {
+		return this.sessionID;
+	}
+	
+	public String getName() {
+		String fullName = this.firstName + this.lastName;
+		return fullName;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
