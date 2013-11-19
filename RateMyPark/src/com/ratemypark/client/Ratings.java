8921deced236925b2d8e9e7740867034ba51e21a
iamwillz <william.zhang.285@gmail.com>
2013-11-19 13:22:40 -0800

    Adding Ratings class, and corresponding unit tests

---
+++ RateMyPark/src/com/ratemypark/client/Ratings.java
@@ -0,0 +1,68 @@
+package com.ratemypark.client;
+
+import java.util.Date;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Rating;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import com.ratemypark.exception.RatingOutOfRangeException;
+
+@PersistenceCapable
+public class Ratings implements IsSerializable{
+	
+	@PrimaryKey
+	@Persistent
+	private String username;
+	
+	@PrimaryKey
+	@Persistent
+	private Long pid;
+	
+	@Persistent
+	private int ratingnumber;
+	
+	@Persistent
+	private Date dateReviewed;
+	
+	private final int MIN_VALUE = 0;
+	private final int MAX_VALUE = 5;
+	
+	public Ratings() {
+		this.dateReviewed= new Date();
+	}
+	
+	public Ratings(Long pid, String username){
+		this();
+		this.pid = pid;
+		this.username = username;		
+	}
+	
+	// Sets the rating value, and returns true if the number was valid, false otherwise
+	public boolean setRating(int num) {
+		if (MIN_VALUE <= num && num <= MAX_VALUE){
+			// Check if rating is in range of 0-5
+			ratingnumber = num;
+			return true;
+		} else {
+			// throw exception that rating we're trying to set it out of range
+//			throw new RatingOutOfRangeException("Rating is out of the range 0-5");
+			return false;
+		}
+	}
+
+	public int getRating() {
+		return this.ratingnumber;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public Long getPid() {
+		return this.pid;
+	}
+}
+
---
+++ RateMyPark/test/com/ratemypark/test/client/RatingsTest.java
@@ -0,0 +1,62 @@
+package com.ratemypark.test.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ratemypark.client.Ratings;
+
+public class RatingsTest {
+
+	Ratings ratings;
+
+	@Before
+	public void setup(){
+		Long pid = new Long(1);
+		String accname = "account1";
+		ratings = new Ratings(pid,accname);
+		
+	}
+	
+	@Test
+	public void testRatingsLongString() {
+		assertEquals(new Long(1),ratings.getPid());
+		assertEquals("account1",ratings.getUsername());
+	}
+	
+	@Test
+	public void testSetValidRating(){
+		assertTrue(ratings.setRating(5));
+		assertTrue(ratings.setRating(4));
+		assertTrue(ratings.setRating(3));
+		assertTrue(ratings.setRating(2));
+		assertTrue(ratings.setRating(1));
+		assertTrue(ratings.setRating(0));
+	}
+	
+	@Test 
+	public void testSetInvalidRating(){
+		assertFalse(ratings.setRating(100));
+		assertFalse(ratings.setRating(-1));
+		assertFalse(ratings.setRating(6));
+		assertFalse(ratings.setRating(10));
+		assertFalse(ratings.setRating(-100));
+	}
+	
+	@Test
+	public void testGetValidRating(){
+		ratings.setRating(5);
+		assertEquals(5,ratings.getRating());
+	}
+	
+	@Test
+	public void testGetValidAfterInvalidRating(){
+		ratings.setRating(5);
+		assertFalse(ratings.setRating(100));
+		assertEquals(5,ratings.getRating());
+	}
+
+
+}
